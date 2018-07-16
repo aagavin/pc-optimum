e81c34eb57f3884ab8a815a8dc06fb3e661b0adf
aagavin <aagavin@gmail.com>
2018-07-16 19:58:14 -0400

    added pdf generation

--- src/index.ts
+++ src/index.ts
@@ -8,24 +8,41 @@ const EMAIL_INPUT: string = "#email";
 const PASS_INPUT: string = "#password";
 const SIGNIN_BUTTON: string = ".button--block";
 
-exports.handler = async (event, context) => {
+const h = exports.handler = async (event, context) => {
     const browser: Browser = await puppeteerLambda.getBrowser({
-        headless: true
+        headless: true,
+        args: ['--no-sandbox']
     });
 
     const page: Page = await browser.newPage();
+    await page.setViewport({ 'width': 1920, 'height': 1080 });
     await page.goto(URL);
     await page.click(EMAIL_INPUT);
+
     await Promise.all([
-        await page.type(EMAIL_INPUT, "hello")
+        await page.type(EMAIL_INPUT, process.env.USERNAME)
     ]);
 
-    await page.click(PASS_INPUT)
+    await page.click(PASS_INPUT)    
     await Promise.all([
-        await page.type(PASS_INPUT, "Password")
+        await page.type(PASS_INPUT.toString(), process.env.PASSWORD)
     ]);
 
-    await page.click(SIGNIN_BUTTON);
+    await Promise.all([
+        await page.click(SIGNIN_BUTTON),
+        await page.waitForNavigation()
+    ]);
+
+    await page.waitFor(1000);
+
+    await page.pdf({path: `pc-points-${(new Date).toDateString().replace(/ /g, '-')}.pdf`, printBackground: true});
+    await page.close();
+    await browser.close();
     
     return event.key1;  // Echo back the first key value
 };
+
+
+h({'key1': 'sdfdsf'}, null)
+.then(val => console.log(val))
+.catch(err => console.log(err))
