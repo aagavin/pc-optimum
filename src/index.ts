ccf39e3e8a1df00937892da05cae2f58cd8b159d
aagavin <aagavin@gmail.com>
2018-07-16 23:42:20 -0400

    updated to send email using nodemailer

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,37 @@
 import { Page, Browser, ElementHandle } from "puppeteer";
 import puppeteerLambda = require('puppeteer-lambda');
+import nodemailer = require('nodemailer');
+import { MailOptions } from "nodemailer/lib/smtp-pool";
 
 console.log('Loading function');
 
+// puppeteer constants
 const URL: string = "https://www.pcoptimum.ca/login";
 const EMAIL_INPUT: string = "#email";
 const PASS_INPUT: string = "#password";
 const SIGNIN_BUTTON: string = ".button--block";
+const ELEMENT_WAIT_FOR: string = ".product-offer .offer__image-element--default, .hero-product-offer .offer__image-element--default";
+const PDF_NAME: string = `pc-points-${(new Date).toDateString().replace(/ /g, '-')}.pdf`;
+
+// nodemailer constants
+let transporter = nodemailer.createTransport({
+    host: process.env.E_HOST,
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.E_USERNAME,
+        pass: process.env.E_PASSWORD
+    }
+});
+
+
+let mailOptions:MailOptions = {
+    from: process.env.E_FROM,
+    to: process.env.E_TO,
+    subject: process.env.E_Subject,
+    text: process.env.E_Subject,
+    attachments: [{path: PDF_NAME}]
+}
 
 const h = exports.handler = async (event, context) => {
     const browser: Browser = await puppeteerLambda.getBrowser({
@@ -20,29 +45,37 @@ const h = exports.handler = async (event, context) => {
     await page.click(EMAIL_INPUT);
 
     await Promise.all([
-        await page.type(EMAIL_INPUT, process.env.USERNAME)
+        await page.type(EMAIL_INPUT, process.env.PC_USERNAME)
     ]);
 
     await page.click(PASS_INPUT)    
     await Promise.all([
-        await page.type(PASS_INPUT.toString(), process.env.PASSWORD)
+        await page.type(PASS_INPUT.toString(), process.env.PC_PASSWORD)
     ]);
 
     await Promise.all([
         await page.click(SIGNIN_BUTTON),
-        await page.waitForNavigation()
+        await page.waitForNavigation({waitUntil: "networkidle0"})
     ]);
 
-    await page.waitFor(1000);
+    // await page.waitForSelector(ELEMENT_WAIT_FOR);
 
-    await page.pdf({path: `pc-points-${(new Date).toDateString().replace(/ /g, '-')}.pdf`, printBackground: true});
+    await page.emulateMedia('screen');
+    await page.evaluate(()=>{
+        (document.getElementsByClassName('menu').item(0) as HTMLElement).style.display = 'None';
+        return;
+    })
+    await page.pdf({path: PDF_NAME, printBackground: true, displayHeaderFooter: false});
     await page.close();
     await browser.close();
+
+    let mail = await transporter.sendMail(mailOptions);
+    console.log('Message sent: %s', mail.messageId);
     
-    return event.key1;  // Echo back the first key value
+    return PDF_NAME;
 };
 
 
-h({'key1': 'sdfdsf'}, null)
+h({}, null)
 .then(val => console.log(val))
 .catch(err => console.log(err))
