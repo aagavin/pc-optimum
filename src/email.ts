4199744baedb8b5670f795f18c55812416125cf5
aagavin <aagavin@gmail.com>
2018-11-03 21:24:28 -0400

    updated transporter

--- src/email.ts
+++ src/email.ts
@@ -2,36 +2,34 @@ import nodemailer = require('nodemailer');
 import { MailOptions } from "nodemailer/lib/smtp-pool";
 
 
-export const sendEmail = async (pdfPath: string): Promise<string> => {
+export const sendEmail = async (pdfPath: string, username: string): Promise<string> => {
 
-    // nodemailer constants
     const transporter = nodemailer.createTransport({
-        host: process.env.E_HOST,
-        port: 465,
-        secure: true,
+        service: "FastMail", // no need to set host or port etc.
         auth: {
             user: process.env.E_USERNAME,
             pass: process.env.E_PASSWORD
         }
-    });
+   });
 
 
     const mailOptions: MailOptions = {
         from: process.env.E_FROM,
         to: process.env.E_TO,
-        subject: process.env.E_Subject,
-        text: process.env.E_Subject,
+        subject: 'PC Optimum points',
+        text: `PC Optimum points for ${username}`,
         attachments: [{ path: pdfPath }]
     }
 
 
     // send email
     console.info('sending mail using');
+    console.info(JSON.stringify(mailOptions));
     try {
         const mail = await transporter.sendMail(mailOptions);
         console.info('Message sent: %s to %s', mail.messageId, process.env.E_TO);
         return mail.messageId;
     }
-    catch (err) { console.log(err) }
+    catch (err) { console.error(err) }
 
 }
