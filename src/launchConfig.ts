53b99bd7c59c57ddae52a0c51575a82ad73f9fc3
aagavin <aagavin@gmail.com>
2018-11-03 21:28:28 -0400

    make device not be able to be detected

--- src/launchConfig.ts
+++ src/launchConfig.ts
@@ -6,7 +6,19 @@
  */
 export const getLaunchConfig = (): object => {
     let launchConfig = {
-        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu', '--single-process', "--proxy-server='direct://'", '--proxy-bypass-list=*']
+        args: [
+            '--disable-gpu',
+            '--single-process',
+            "--proxy-server='direct://'",
+            '--proxy-bypass-list=*',
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-infobars',
+            '--window-position=0,0',
+            '--ignore-certifcate-errors',
+            '--ignore-certifcate-errors-spki-list',
+            '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"'
+        ]
     }
 
     if (typeof process.env.NODE_ENV === 'undefined') {
