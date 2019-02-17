88b1cf76ea09cfce8f9c33a3a4a33c3a820a9798
aagavin <aagavin@gmail.com>
2019-02-16 20:31:27 -0500

    added compile and package scripts

--- buildCi.sh
+++ buildCi.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 #
 # "build": "npm run clean && export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true && npm install --production && npm install typescript --no-save && tsc && npm uninstall typescript",
+# "package": "npm run build && zip -r lambda.zip node_modules && cd ./build && zip -ru ../lambda.zip . && cd .. && zip -ju lambda.zip ./headless-chromium",
 #
-curl https://github.com/adieuadieu/serverless-chrome/releases/download/v1.0.0-55/stable-headless-chromium-amazonlinux-2017-03.zip -o chromium.zip
-unzip chromium.zip
+
+# 
+# compile
+# 
+chromefile="chromium.zip"
 npm run clean
+curl -L https://github.com/adieuadieu/serverless-chrome/releases/download/v1.0.0-55/stable-headless-chromium-amazonlinux-2017-03.zip -o $chromefile
+unzip $chromefile
 export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
 export NODE_ENV=production
 npm ci --only=production
 npm install typescript --no-save
 tsc
 npm uninstall typescript
+
+#
+# package
+# 
+zip -r lambda.zip node_modules
+cd ./build
+zip -ru ../lambda.zip .
+cd ..
+zip -ju lambda.zip ./headless-chromium
