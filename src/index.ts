76322f268ef1cbe9e12902f87dc9cc442881a1a7
aagavin <aagavin@gmail.com>
2019-04-19 20:39:05 -0400

    type hint removed commented code

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,8 @@ import { sendEmail } from "./email";
 
 console.log('Loading function');
 
-exports.pcOptimum = async (event) => {
+exports.pcOptimum = async (event: { username: string; password: string; }) => {
     const pdf_path: string = await getPDFPath(event.username, event.password);
-    // return res.send(await sendEmail(pdf_path));
     return {
         emailId: await sendEmail(pdf_path, event.username),
         sentTo: process.env.E_TO
