5faf871d0e0433caa04d70fceb9a643135656c10
aagavin <aagavin@gmail.com>
2020-01-26 17:21:09 -0500

    working config

--- src/puppeteer.ts
+++ src/puppeteer.ts
@@ -2,54 +2,59 @@ import { Page, Browser } from "puppeteer";
 import puppeteer = require('puppeteer');
 import { getLaunchConfig } from "./launchConfig";
 
+const getRandom = () => Math.random() * (100 - 50) + 50;
+
 export const getPDFPath = async (username: string, password: string) => {
-    // puppeteer constants
-    const URL: string = "https://www.pcoptimum.ca/login";
-    const EMAIL_INPUT: string = "#email";
-    const PASS_INPUT: string = "#password";
-    const PDF_PATH: string = `/tmp/pc-points-${username}-${(new Date()).toDateString().replace(/ /g, '-')}.pdf`;
+  // puppeteer constants
+  const URL: string = "https://www.pcoptimum.ca/login";
+  const EMAIL_INPUT: string = "#email";
+  const PASS_INPUT: string = "#password";
+  const PDF_PATH: string = `/tmp/pc-points-${username}-${(new Date()).toDateString().replace(/ /g, '-')}.png`;
 
 
+  try {
     const browser: Browser = await puppeteer.launch(getLaunchConfig());
 
     let page: Page;
     try {
-        console.info('opening new tab');
-        page = await browser.newPage();
-        await page.setCacheEnabled(false);
-        await page.setViewport({ 'width': 1920, 'height': 1080 });
+      console.info('opening new tab');
+      page = await browser.newPage();
+      await page.setCacheEnabled(false);
+      await page.setViewport({ 'width': 1920, 'height': 1080 });
     }
     catch (err) {
-        console.error(err);
+      console.error(err);
     }
 
     console.info('going to url');
 
-    await page.goto(URL, { waitUntil: 'networkidle0' });
+    await page.goto(URL);
     console.info('went to url');
 
     console.info('logging in to pc w/ %s', username);
-    await page.type(EMAIL_INPUT, username);
+    await page.focus(EMAIL_INPUT);
+    await page.keyboard.type(username, { delay: getRandom() });
     console.info('typed username');
-    await page.waitFor(200);
+
     console.info('typeing password');
-    await page.type(PASS_INPUT, password);
+    await page.focus(PASS_INPUT);
+    await page.keyboard.type(password, { delay: getRandom() });
     console.info('typed password');
-    await page.waitFor(200);
-    await page.focus('#login > button');
 
     console.info('clicking submit button');
+    await page.focus('#login > button');
     await Promise.all([
-        page.waitForNavigation({ waitUntil: 'networkidle0' }),
-        page.click('#login > button')
+      page.waitForNavigation(),
+      page.click('#login > button')
     ]);
 
     await page.waitFor(3000);
 
     console.info('getting pdf');
-    await page.emulateMedia('screen');
+    await page.emulateMediaType('screen');
+
     await page.addStyleTag({
-        content: `
+      content: `
       *,
       *::after,
       *::before {
@@ -64,25 +69,27 @@ export const getPDFPath = async (username: string, password: string) => {
     `
     });
     await page.evaluate((username: string) => {
-        const menu: HTMLCollection = document.getElementsByClassName('menu');
-        const siteFooter: HTMLCollection = document.getElementsByClassName('site-footer');
-        const promotedAction: HTMLCollection = document.getElementsByClassName('promoted-action');
-        const offersHeaderPoints: HTMLCollection = document.getElementsByClassName('offers-header-points__value');
+      const menu: HTMLCollection = document.getElementsByClassName('menu');
+      const siteFooter: HTMLCollection = document.getElementsByClassName('site-footer');
+      const promotedAction: HTMLCollection = document.getElementsByClassName('promoted-action');
+      const offersHeaderPoints: HTMLCollection = document.getElementsByClassName('offers-header-points__value');
+      const siteSidebar: Element = document.querySelector('section.checklist-container');
 
-        const elements = document.querySelectorAll('li.invisible-offer');
-        for (let i = 0; i < elements.length; i++) {
-            const li = elements[i] as HTMLElement;
-            li.setAttribute('tabindex', i+'');
-            li.focus();
-            li.click(); 
-        }
+      const elements = document.querySelectorAll('li.invisible-offer');
+      for (let i = 0; i < elements.length; i++) {
+        const li = elements[i] as HTMLElement;
+        li.setAttribute('tabindex', i + '');
+        li.focus();
+        li.click();
+      }
 
-        // remove uneeded items
-        menu.length != 0 ? (menu.item(0) as HTMLElement).style.display = 'None' : '';
-        siteFooter.length != 0 ? (siteFooter.item(0) as HTMLElement).style.display = 'None' : '';
-        promotedAction.length != 0 ? (promotedAction.item(0) as HTMLElement).style.display = 'None' : '';
-        offersHeaderPoints.length != 0 ? (offersHeaderPoints.item(0) as HTMLElement).innerText = username.split('@')[0] : '';
-        return;
+      // remove uneeded items
+      menu.length != 0 ? (menu.item(0) as HTMLElement).style.display = 'None' : '';
+      siteFooter.length != 0 ? (siteFooter.item(0) as HTMLElement).style.display = 'None' : '';
+      promotedAction.length != 0 ? (promotedAction.item(0) as HTMLElement).style.display = 'None' : '';
+      offersHeaderPoints.length != 0 ? (offersHeaderPoints.item(0) as HTMLElement).innerText = username.split('@')[0] : '';
+      (siteSidebar as HTMLElement).style.display = 'None';
+      return;
     }, username);
     console.log('getting pdf');
     await page.pdf({ path: PDF_PATH, printBackground: true, displayHeaderFooter: false });
@@ -91,5 +98,11 @@ export const getPDFPath = async (username: string, password: string) => {
     await browser.close();
 
     return PDF_PATH;
+
+  }
+  catch (err) {
+    console.error(err);
+  }
+
 
 }
